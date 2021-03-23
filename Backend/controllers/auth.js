81d6666ff27ac84898c97b7b5afabc5ea17f1f4f
vicky <srivicky2000@gmail.com>
2021-03-23 16:07:26 +0530

    Added FAKE JWT admin login

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,3 +1,15 @@
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
+const { v4: uuidv4 } = require('uuid');
+
 exports.adminAuth = (req, res) => {
-    console.log(res.body);
+    const PASSCODE = req.body.passcode;
+    if (PASSCODE == '8072002769' || PASSCODE == '9597621113') {
+        const token = jwt.sign({ _id: uuidv4() }, process.env.SECRET);
+        res.cookie("ADMIN", token, { expire: new Date() + 99999 });
+        return res.json({
+            user: uuidv4()
+        })
+    }
 }
