79ad840d27947149e0a9c6f3b3cf6a4bf8a9bddc
vicky <srivicky2000@gmail.com>
2021-03-23 15:48:11 +0530

    Project setup done

---
+++ Backend/app.js
@@ -0,0 +1,41 @@
+  require("dotenv").config();
+
+  const mongoose = require("mongoose");
+  const express = require("express");
+  const app = express();
+  const bodyParser = require("body-parser");
+  const cookieParser = require("cookie-parser");
+  const cors = require("cors");
+  const chalk = require('chalk');
+
+  //PORT
+  const port = process.env.PORT || 8000;
+
+  //Middlewares
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use(cors());
+
+  //DB Connection
+  mongoose
+      .connect(process.env.DATABASE, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false
+      })
+      .then(() => {
+          console.log("DB CONNECTED");
+      });
+
+
+
+  app.get("/", (req, res) => {
+      res.json({
+          msg: 'Vicky'
+      })
+  });
+
+  app.listen(port, () => {
+      console.log("Server started");
+  })
